e3afb11ccf75119fb6f4fd04bf3fdb801dbe29d4
Enda Barrett <barrettenda@gmail.com>
2022-11-24 14:21:59 +0000

    Changed the comments

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,7 +4,7 @@
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
 */
 //
-// Cool Scripts
+// Adding to this branch
 // 
 
 window.addEventListener('DOMContentLoaded', event => {
